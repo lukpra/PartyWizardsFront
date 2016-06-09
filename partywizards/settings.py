53d6aa5102fa53cb2249e29f17f1b186aae89dce
Deinonzch <slagol@st.amu.edu.pl>
2016-06-09 22:00:30 +0200

    Add wizapp.apps.WizappConfig to partywizards/setting.py

--- partywizards/settings.py
+++ partywizards/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'wizapp.apps.WizappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
