d2b2bc88392f4ccb252e74800f1bdf4012d33710
Deinonzch <slagol@st.amu.edu.pl>
2016-06-10 00:06:03 +0200

    add blank and __str__ to model

--- wizapp/models.py
+++ wizapp/models.py
@@ -2,32 +2,50 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Drink(models.Model):
-    name =  models.CharField(max_length=250)
+    name =  models.CharField(max_length=250, blank=False)
     recipe = models.CharField(max_length=1000)
-    picture = models.FileField()
+    picture = models.FileField(blank=False)
+
+    def __str__(self):
+        return self.name
 
 class Ingredient(models.Model):
-    name =  models.CharField(max_length=250)
+    name =  models.CharField(max_length=250, blank=False)
     kind = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
 
 class Tool(models.Model):
-    name = models.CharField(max_length=250)
+    name = models.CharField(max_length=250, blank=False)
     kind = models.CharField(max_length=250)
-    picture = models.FileField()
+    picture = models.FileField(blank=False)
+
+    def __str__(self):
+        return self.name
 
 class Decoration(models.Model):
-    name = models.CharField(max_length=250)
-    picture = models.FileField()
+    name = models.CharField(max_length=250, blank=False)
+    picture = models.FileField(blank=False)
+
+    def __str__(self):
+        return self.name
 
 class Barcode(models.Model):
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-    barcode = models.IntegerField()
+    barcode = models.IntegerField(blank=False)
+
+    def __str__(self):
+        return self.barcode
 
 class ListIngredients(models.Model):
     drink = models.ForeignKey(Drink, on_delete=models.CASCADE)
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
     quantity = models.FloatField()
     kindMeasure = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.quantity + ' - ' + self.kindMeasure
 
 class ListTools(models.Model):
     drink = models.ForeignKey(Drink, on_delete=models.CASCADE)
@@ -42,24 +60,38 @@ class Rating(models.Model):
     user = models.ForeignKey(User)
     rating = models.IntegerField();
 
+    def __str__(self):
+        return self.rating
+
 class Comment(models.Model):
     drink = models.ForeignKey(Drink, on_delete=models.CASCADE)
     user = models.ForeignKey(User)
-    comment = models.CharField(max_length=1000)
+    comment = models.CharField(max_length=1000, blank=False)
     date = models.DateField(auto_now_add=True)
     delete = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.comment + ' - ' + self.date
+
 class Answer(models.Model):
     answer_comment = models.ForeignKey(Comment)
-    comment = models.CharField(max_length=1000)
+    comment = models.CharField(max_length=1000, blank=False)
     date = models.DateField(auto_now_add=True)
     delete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.comment + ' - ' + self.date
+
 
 class Ban(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     start_date = models.DateField()
     end_date = models.DateField()
     perm = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.start_date + ' - ' + self.end_date
+
 
 class myUser(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
