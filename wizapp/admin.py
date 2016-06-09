e31ef5bdb90fc96b578bff22e4a08d9dd080b5cb
lukpra <praza99+git@gmail.com>
2016-06-09 23:44:56 +0200

    Database sample data

--- wizapp/admin.py
+++ wizapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Drink, Ingredient, Tool, Rating, Decoration, Barcode, ListIngredients, ListDecoration, ListTools, Comment, Answer, Ban, myUser
 
 # Register your models here.
+admin.site.register(Drink)
+admin.site.register(Tool)
+admin.site.register(Ingredient)
+admin.site.register(Decoration)
+admin.site.register(Barcode)
+admin.site.register(ListIngredients)
+admin.site.register(ListDecoration)
+admin.site.register(ListTools)
+admin.site.register(Ban)
+admin.site.register(Answer)
+admin.site.register(Comment)
+admin.site.register(Rating)
