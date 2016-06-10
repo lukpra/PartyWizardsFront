fb7fadf72aab587a4a72e10dde0bbd31422b50e3
lukpra <praza99+git@gmail.com>
2016-06-10 11:15:44 +0200

    changes

--- wizapp/views.py
+++ wizapp/views.py
@@ -1,10 +1,32 @@
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
+from .models import Barcode
+from .models import Drink
 
 class IndexView(generic.TemplateView):
     template_name = 'index.html'
 
+class BarcodeView(generic.ListView):
+    context_object_name = 'barcodes_all'
+    model = Barcode
+    template_name = 'barcodes.html'
+
+class BarcodeAdd(CreateView):
+    model = Barcode
+    fields = ['barcode', 'ingredient']
+    success_url = reverse_lazy('wizapp:barcodes')
+
+class AddDrinkView(CreateView):
+    model = Drink
+    fields = ['name', 'recipe', 'picture']
+
+class DrinksView(generic.ListView):
+   template_name = 'wizapp/drinks.html'
+   context_object_name = 'all_drinks'
+
+   def get_queryset(self):
+       return Drink.objects.all()
 '''
 
 def index(request):
