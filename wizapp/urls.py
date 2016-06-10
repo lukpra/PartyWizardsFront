ac17260fb6dea3925a424f97d5f7684cff6ad111
lukpra <praza99+git@gmail.com>
2016-06-10 10:23:32 +0200

    Barcode view

--- wizapp/urls.py
+++ wizapp/urls.py
@@ -4,9 +4,12 @@ from django.views.generic import TemplateView
 
 
 
-app_name = 'wizzapp'
+app_name = 'wizapp'
 
 urlpatterns = [
     # / - index
     url(r'^$', TemplateView.as_view(template_name="index.html"), name='index'),
+
+    # /barcode/add/
+    url(r'^barcode/$', views.BarcodeView.as_view(),name="barcodes")
 ]
